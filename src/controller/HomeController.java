72bc76744bb33d18fbd95378f19e36321d584b60
neo-23 <saimabintem@gmail.com>
2021-03-21 04:00:00 +0600

    Fix some errors on build process

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,16 +1,23 @@
-package QuickSave.controller;
+package quickSave.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class HomeController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
+        if (session.getAttribute("userInfo") != null) {
+            request.getRequestDispatcher("WEB-INF/view/home.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
